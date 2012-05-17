cc847603e15b77564ec467e578d2a44c730799db
Andrew Premdas <apremdas@yahoo.co.uk>
2012-05-17 14:44:43 +0100

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyTime::Application.config.secret_token = '49d3e99acf901696744c2af5f0737ab5ccc118090751cda717e9d61a2d6929341ad3be5ef8f02ff56f00a0a75858b4a261a4a15c528dd2395274e32f7b92a063'
