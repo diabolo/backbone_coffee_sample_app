27e911c5d9eeff43b467fad4f9969826a9eb492f
Andrew Premdas <apremdas@yahoo.co.uk>
2012-05-17 16:01:44 +0100

    Installed backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//
+//= require underscore
+//= require backbone
+//
+//= require party_time_app
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+//= require_tree ./templates
 //= require_tree .
